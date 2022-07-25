6c9dd511caad4aaf54da638bde6e47cfef1b9ae7
tangusjr <kipngenogilbert96@gmail.com>
2022-07-25 12:05:56 +0300

    Done.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { directors } from "../data";
 
-function Home() {
-  return <div>{/*{code here}*/}</div>;
+function Directors() {
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director) => {
+        return (
+          <div key={director.name}>
+            <h3>Name: {director.name}</h3>
+            <p>Movies: </p>
+            <ul>
+              {director.movies.map((movie, i) => (
+                <li key={movie + i}> {movie}</li>
+              ))}
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
-export default Home;
+export default Directors;
